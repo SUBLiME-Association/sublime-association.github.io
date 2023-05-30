483c04e958390baa5c22d844dbec3aceae3576d0
SUP2Ak <cormier.wesley@gmail.com>
2023-05-30 22:21:31 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { Toast, toastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
+
+	function toastDemo(): void {
+		const t: ToastSettings = {
+			message: '👋 Hello and welcome to Skeleton.',
+			callback: (response) => console.log(response)
+		};
+		toastStore.trigger(t);
+	}
+</script>
+
+<Toast />
+<div class="container">
+	<button on:click={toastDemo}> Test notify </button>
+</div>
